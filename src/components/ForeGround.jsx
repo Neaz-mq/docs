63d775a0f859447c8b2939cd74b4c668fe50dacf
Neaz-mq <neazmorshed666@gmail.com>
2024-01-06 21:52:36 +0600

    card added

--- src/components/ForeGround.jsx
+++ src/components/ForeGround.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Card from './Card'
 
 function ForeGround() {
   return (
     
-    <div className='fixed top-0 left-0 z-[3] w-full h-full '></div>
+    <div className='fixed top-0 left-0 z-[3] w-full h-full'>
+
+        <Card></Card>
+    </div>
   )
 }
 
