a46dae3764dab5265b3d3d60960ad6bd18e0d6e1
Neaz-mq <neazmorshed666@gmail.com>
2024-01-07 14:37:04 +0600

    working card content by dynamic

--- src/components/ForeGround.jsx
+++ src/components/ForeGround.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Card'
 
 function ForeGround() {
+
+  const data = [
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.", filesize: ".9mb", close: true, tag: {isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+   },
+
+   {
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.", filesize: ".9mb", close: true, tag: {isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+ },
+
+ {
+  desc: "Lorem ipsum dolor sit amet consectetur adipisicing.", filesize: ".9mb", close: true, tag: {isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+},
+
+
+
+  ];
+
+
+
   return (
     
-    <div className='fixed top-0 left-0 z-[3] w-full h-full'>
+    <div className='fixed top-0 left-0 z-[3] w-full h-full flex gap-5 flex-wrap'>
 
-        <Card></Card>
+      {data.map((item, index)=> (
+        <Card data={item}/>
+      ))}
+      
     </div>
   )
 }
